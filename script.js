5d1221e768d4fe12129606c683d11adf290cc7dc
eadtools <85564696+eadtools@users.noreply.github.com>
2021-06-25 10:08:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 function exibeApp(){
     let rowApp = document.getElementById('colApp');
     let texto = '';
-
-    let dados = JSON.parse(this.responseText);
-    for (i=0; i<dados.aplicativos.length;i++){
-        let app = dados.aplicativos[i];
+    let pesquisa = document.getElementById('pesquisa_app');
+    let dadosp = JSON.parse(this.responseText);
+    dadosp = dadosp.aplicativos;
+    let filtroNota = document.getElementById('filtroNota');
+    let filtroCategoria = document.getElementById('filtroCategoria');
+    let filtroDispositivo = document.getElementById('filtroDispositivo');
+    let filtroPago = document.getElementById('filtroPago');
+    let dados = dadosp.filter(item => item.Nota > filtroNota.options[filtroNota.selectedIndex].value);
+    dados = dados.filter(item => item.Dispositivo.includes(filtroDispositivo.options[filtroDispositivo.selectedIndex].value));
+    dados = dados.filter(item => item.Categoria.includes(filtroCategoria.options[filtroCategoria.selectedIndex].value));
+    dados = dados.filter(item => item.Pago.includes(filtroPago.options[filtroPago.selectedIndex].value));
+    if(pesquisa.value != ""){
+        dados = dados.filter(item => item.Nome.includes(pesquisa.value));
+    }
+    for (i=0; i<dados.length;i++){
+        let app = dados[i];
         let dispcomplete = '';
-        for(j=1; j<app.Dispositivo.length;j++){
+        for(j=2; j<app.Dispositivo.length;j++){
             dispcomplete = dispcomplete + `/${app.Dispositivo[j]}`;
         }
         texto = texto + `
@@ -19,7 +31,7 @@ function exibeApp(){
             </div>
             <div class="col-md-9 col-12" id="infoapli">
                 <h2>${app.Nome}</h2>
-                <p><span>Dispositivo:</span> ${app.Dispositivo[0]+dispcomplete}</p>
+                <p><span>Dispositivo:</span> ${app.Dispositivo[1]+dispcomplete}</p>
                 <p><span>Descrição:</span> ${app.Descrição}</p>
                 <div class="mb-3">
                     <a class="btn btn-success" href="#" role="button">Ver mais</a>
@@ -33,7 +45,31 @@ function exibeApp(){
     rowApp.innerHTML = texto;
 }
 
+function initApp(){
+    let rowApp = document.getElementById('itemsdocar');
+    let texto = '';
+    let dadosp = JSON.parse(this.responseText);
+    dados = dadosp.aplicativos;
+    for (i=0; i<4;i++){
+        let app = dados[i];
+        texto = texto + `
+            <div class="col-sm-3">
 
+            <div class="card">
+            <div class="card-body">
+                <img src="${app.img}" height="100" width="100" alt="">
+                <h5 class="card-title">${app.Nome}</h5>
+                <p class="card-text"><span>Descrição:</span> ${app.Descrição} <br><span>
+                    Nota:</span> ${app.Nota}/10</p>
+                <a href="#" class="btn btn-success">Ver</a>
+            </div>
+            </div>
+        </div>
+        `;
+    };
+
+    rowApp.innerHTML = texto;
+}
 
 function listarApps (){
     let xhr = new XMLHttpRequest ();
@@ -42,4 +78,12 @@ function listarApps (){
     xhr.send();
 }
 
+function destaqueApps (){
+    let xhr2 = new XMLHttpRequest ();
+    xhr2.onload = initApp;
+    xhr2.open('GET','db.json');
+    xhr2.send();
+}
+
 document.onload = listarApps();
+document.onload = destaqueApps();
