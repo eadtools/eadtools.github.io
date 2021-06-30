c30c4bd7ba4596924edec5c3eb43d6b19f9b69a8
eadtools <85564696+eadtools@users.noreply.github.com>
2021-06-29 21:50:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -90,9 +90,20 @@ function AppPage(id2){
     xhr3.onload = function Page(){
         let w = window.open('app.html', '_blank');
         let texto = '';
+        let texto2 = '';
+        for (var i = 0; i < db_comentarios.comentarios.length; i++){
+            let c = db_comentarios.comentarios[i]
+            if(c.id_app == id2){
+                texto2 = texto2+`
+                <h5>${c.autor}</h5>
+                <textarea disabled name="comment" id="" cols="10" rows="5">${c.texto}</textarea>
+                `;
+            }
+        }
         let dispcomplete = '';
         let dadosp = JSON.parse(this.responseText);
         dados = dadosp.aplicativos;
+
         let app = dados[id2-1];
         for(j=2; j<app.Dispositivo.length;j++){
             dispcomplete = dispcomplete + `/${app.Dispositivo[j]}`;
@@ -122,7 +133,7 @@ function AppPage(id2){
                               <a class="nav-link" aria-current="page" href="index.html">Home</a>
                               <a class="nav-link active" aria-current="page" href="apli.html">Aplicações</a>
                               <a class="nav-link" aria-current="page" href="#">Saiba mais</a>
-                              <a class="nav-link" aria-current="page" href="login.html">Login</a>
+                              <a class="nav-link" aria-current="page" id="loglink" href="login.html">Login</a>
                             </div>
                           </div>
                         </div>
@@ -150,6 +161,12 @@ function AppPage(id2){
                                     <p>Vá para a página da aplicação clicando aqui: <a href="${app.local}">${app.local}</a></p>
                                     <h3>Dicas de uso</h3>
                                     <p>${app.dica}</p>
+                                    <div class="row" id="commentarea">
+                                    <h3>Comentários</h3>
+                                    <textarea name="comment" id="mycomment" cols="10" rows="5"></textarea>
+                                    <a href="#" id="comentar" onclick="comentar(${id2})" class="btn btn-success">Comentar</a>
+                                    ${texto2}
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -160,8 +177,8 @@ function AppPage(id2){
           </body>
         </html>
         `;
-        //w.document.getElementById('colaApp').innerHTML = texto;
         w.document.write(`<p>${texto}</p>`);
+        verifyLogged();
     };
     xhr3.open('GET','db.json');
     xhr3.send();
@@ -169,3 +186,211 @@ function AppPage(id2){
 
 document.onload = listarApps();
 document.onload = destaqueApps();
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+//
+//
+// Disciplina: Trabalho Interdisciplinar - Aplicações Web
+// Professor: Rommel Vieira Carneiro (rommelcarneiro@gmail.com)
+//
+// Código LoginApp utilizado como exemplo para alunos de primeiro período 
+
+
+// Página inicial de Login
+const LOGIN_URL = "login.html";
+
+//functions latter added
+
+function verifyLogged(){
+    var logado = localStorage.getItem('logado');
+    if (!logado) {  
+        logado = 0;
+        localStorage.setItem('logado', JSON.stringify (logado));
+    }
+    else  {
+        logado = JSON.parse(logado);
+        let navlog = document.getElementById("loglink");
+        var comentar_permit = document.getElementById("mycomment");
+        var comentar_button = document.getElementById("comentar");
+        if(logado == 1){
+            let navlog = document.getElementById("loglink");
+            var comentar_permit = document.getElementById("mycomment");
+            navlog.innerText = "Sair";
+            navlog.setAttribute("href","index.html");
+            navlog.setAttribute("onclick","logoutUser ()");
+        }
+        else{
+            navlog.innerText = "Login";
+            navlog.setAttribute("href","login.html");
+            navlog.setAttribute("onclick","");
+            comentar_permit.setAttribute("disabled", "true")
+            comentar_button.setAttribute("disabled","true");
+        }
+    }
+};
+
+// Objeto para o banco de dados de usuários baseado em JSON
+var db_usuarios = {};
+var db_comentarios = {};
+// Objeto para o usuário corrente
+var usuarioCorrente = {};
+
+// Comentarios
+const commentsIniciais = {
+    comentarios: [
+        { "id": 14, "id_app": 1, "autor": "Ana Paula K.", "texto": "O melhor aplicativo que utilizei até agora. Muito útil para dividir as turmas em grupos, com a função de canais separados."},
+        { "id": 16, "id_app": 1, "autor": "Roberto Mancini", "texto": "Um bom aplicativo. Recomendo que se utilize a versão para desktop, pois trava menos."},
+    ]
+};
+
+// função para gerar códigos randômicos a serem utilizados como código de usuário
+// Fonte: https://stackoverflow.com/questions/105034/how-to-create-guid-uuid
+function generateUUID() { // Public Domain/MIT
+    var d = new Date().getTime();//Timestamp
+    var d2 = (performance && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16;//random number between 0 and 16
+        if(d > 0){//Use timestamp until depleted
+            r = (d + r)%16 | 0;
+            d = Math.floor(d/16);
+        } else {//Use microseconds since page-load if supported
+            r = (d2 + r)%16 | 0;
+            d2 = Math.floor(d2/16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
+
+
+// Dados de usuários para serem utilizados como carga inicial
+const dadosIniciais = {
+    usuarios: [
+        { "id": generateUUID (), "login": "admin", "senha": "123", "nome": "Administrador do Sistema", "email": "admin@abc.com"},
+        { "id": generateUUID (), "login": "user", "senha": "123", "nome": "Usuario Comum", "email": "user@abc.com"},
+    ]
+};
+
+
+// Inicializa o usuarioCorrente e banco de dados de usuários da aplicação de Login
+function initLoginApp () {
+    // PARTE 1 - INICIALIZA USUARIOCORRENTE A PARTIR DE DADOS NO LOCAL STORAGE, CASO EXISTA
+    usuarioCorrenteJSON = sessionStorage.getItem('usuarioCorrente');
+    if (usuarioCorrenteJSON) {
+        usuarioCorrente = JSON.parse (usuarioCorrenteJSON);
+    }
+    
+    // PARTE 2 - INICIALIZA BANCO DE DADOS DE USUÁRIOS
+    // Obtem a string JSON com os dados de usuários a partir do localStorage
+    var usuariosJSON = localStorage.getItem('db_usuarios');
+    var commentsJSON = localStorage.getItem('db_comentarios');
+    // Verifica se existem dados já armazenados no localStorage
+    if (!usuariosJSON) {  // Se NÃO há dados no localStorage
+        
+        // Informa sobre localStorage vazio e e que serão carregados os dados iniciais
+        alert('Dados de usuários não encontrados no localStorage. \n -----> Fazendo carga inicial.');
+
+        // Copia os dados iniciais para o banco de dados 
+        db_usuarios = dadosIniciais;
+
+        // Salva os dados iniciais no local Storage convertendo-os para string antes
+        localStorage.setItem('db_usuarios', JSON.stringify (dadosIniciais));
+    }
+    else  {  // Se há dados no localStorage
+        
+        // Converte a string JSON em objeto colocando no banco de dados baseado em JSON
+        db_usuarios = JSON.parse(usuariosJSON);    
+    }
+
+    if (!commentsJSON) {  // Se NÃO há dados no localStorage
+        
+        // Informa sobre localStorage vazio e e que serão carregados os dados iniciais
+        alert('Dados de comentários não encontrados no localStorage. \n -----> Fazendo carga inicial.');
+
+        // Copia os dados iniciais para o banco de dados 
+        db_comentarios = commentsIniciais;
+
+        // Salva os dados iniciais no local Storage convertendo-os para string antes
+        localStorage.setItem('db_comentarios', JSON.stringify (commentsIniciais));
+    }
+    else  {  // Se há dados no localStorage
+        
+        // Converte a string JSON em objeto colocando no banco de dados baseado em JSON
+        db_comentarios = JSON.parse(commentsJSON);    
+    }
+};
+
+
+// Verifica se o login do usuário está ok e, se positivo, direciona para a página inicial
+function loginUser (login, senha) {
+    
+    // Verifica todos os itens do banco de dados de usuarios 
+    // para localizar o usuário informado no formulario de login
+    for (var i = 0; i < db_usuarios.usuarios.length; i++) {
+        var usuario = db_usuarios.usuarios[i];
+        
+        // Se encontrou login, carrega usuário corrente e salva no Session Storage
+        if (login == usuario.login && senha == usuario.senha) {
+            logado = 1;
+            localStorage.setItem('logado', JSON.stringify (logado));
+            //
+            usuarioCorrente.id = usuario.id;
+            usuarioCorrente.login = usuario.login;
+            usuarioCorrente.email = usuario.email;
+            usuarioCorrente.nome = usuario.nome;
+            
+            // Salva os dados do usuário corrente no Session Storage, mas antes converte para string
+            sessionStorage.setItem ('usuarioCorrente', JSON.stringify (usuarioCorrente));
+
+            // Retorna true para usuário encontrado
+            return true;
+        }
+    }
+
+    // Se chegou até aqui é por que não encontrou o usuário e retorna falso
+    return false;
+}
+
+// Apaga os dados do usuário corrente no sessionStorage
+function logoutUser () {
+    logado = 0;
+    localStorage.setItem('logado', JSON.stringify (logado));
+    let navlog = document.getElementById("loglink");
+    navlog.innerText = "Login";
+    usuarioCorrente = {};
+    sessionStorage.setItem ('usuarioCorrente', JSON.stringify (usuarioCorrente));
+    window.location = LOGIN_URL;
+}
+
+function addUser (nome, login, senha, email) {
+    
+    // Cria um objeto de usuario para o novo usuario 
+    let newId = generateUUID ();
+    let usuario = { "id": newId, "login": login, "senha": senha, "nome": nome, "email": email };
+    
+    // Inclui o novo usuario no banco de dados baseado em JSON
+    db_usuarios.usuarios.push (usuario);
+
+    // Salva o novo banco de dados com o novo usuário no localStorage
+    localStorage.setItem('db_usuarios', JSON.stringify (db_usuarios));
+}
+
+
+function comentar(id3) {
+    var texto = document.getElementById('mycomment').value;
+    var autor = JSON.parse(sessionStorage.getItem('usuarioCorrente')).nome;
+    let newId = generateUUID ();
+    let comentario = { "id": newId, "id_app": id3, "autor": autor, "texto": texto};
+    db_comentarios.comentarios.push (comentario);
+    localStorage.setItem('db_comentarios', JSON.stringify (db_comentarios));
+    AppPage(id3);
+}
+
+//function setUserPass () {
+
+//}
+
+// Inicializa as estruturas utilizadas pelo LoginApp
+initLoginApp ();
+document.onload = verifyLogged();
